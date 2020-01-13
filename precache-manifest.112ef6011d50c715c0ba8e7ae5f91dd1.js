dc86a6345691baf146fd007a69ddde2f4f0b6988
Attila Sasvari <asasvari@apache.org>
2020-01-13 22:31:51 +0000

    Updates

---
+++ precache-manifest.112ef6011d50c715c0ba8e7ae5f91dd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd65518e5252665f23f2ed6a4ca1807",
+    "url": "/tsreact/index.html"
+  },
+  {
+    "revision": "466406b9a68dfcbcecb3",
+    "url": "/tsreact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "192b6c028cf1f4322941",
+    "url": "/tsreact/static/js/2.061f4957.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tsreact/static/js/2.061f4957.chunk.js.LICENSE"
+  },
+  {
+    "revision": "466406b9a68dfcbcecb3",
+    "url": "/tsreact/static/js/main.1e71d8ba.chunk.js"
+  },
+  {
+    "revision": "e7ba00445f6d2ee8392d",
+    "url": "/tsreact/static/js/runtime-main.d03670c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tsreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
